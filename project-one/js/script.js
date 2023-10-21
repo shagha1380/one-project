d45308790bfc52e505acc55ed75d890bdda8a53c
shagha <shaghayegh1380gh9@gmail.com>
2023-10-21 20:37:20 +0330

    done

--- project-one/js/script.js
+++ project-one/js/script.js
@@ -8,3 +8,19 @@
         navMenuBtn.addEventListener("click", navToggleFunc);
         navCloseBtn.addEventListener("click", navToggleFunc);
 
+        /* ============ THEME TOGGLE VARIABLES =============  */
+        const themeBtn = document.querySelectorAll(".theme-btn");
+
+        for(let i = 0; i < themeBtn.length; i++) {
+                themeBtn[i].addEventListener("click" , function(){
+                        document.body.classList.toggle("light-theme");
+                        document.body.classList.toggle("dark-theme");
+
+        for(let j = 0; j < themeBtn.length; j++) {
+                themeBtn[j].classList.toggle("light");
+                themeBtn[j].classList.toggle("dark");
+                        }
+                });
+        }
+
+
